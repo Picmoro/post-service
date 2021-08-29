3ee2e6644e452c2e21a97ae71c34e7bb257e6374
Jaime Zamora <kratov@protonmail.com>
2021-08-29 13:35:39 -0500

    post-service init

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+require('colors');
+
+const app =  express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+})
+
+app.post('/posts', (req, res) => {
+    console.log("\nCreating new post".bgYellow.black);
+    const id = randomBytes(4).toString('hex');
+    console.log(`Id: ${id}`.bgYellow.black);
+    const {title} = req.body;
+    posts[id] = {
+        id, title
+    }
+    console.log(`Created: ${JSON.stringify(posts)}`.bgYellow.black);
+    res.status(201).send(posts[id]);
+})
+
+app.listen(4000, () => {
+    console.info('\n' +
+        '██████   ██████  ███████ ████████       ███████ ███████ ██████  ██    ██ ██  ██████ ███████ \n' +
+        '██   ██ ██    ██ ██         ██          ██      ██      ██   ██ ██    ██ ██ ██      ██      \n' +
+        '██████  ██    ██ ███████    ██    █████ ███████ █████   ██████  ██    ██ ██ ██      █████   \n' +
+        '██      ██    ██      ██    ██               ██ ██      ██   ██  ██  ██  ██ ██      ██      \n' +
+        '██       ██████  ███████    ██          ███████ ███████ ██   ██   ████   ██  ██████ ███████ \n' +
+        '                                                                                            \n' +
+        '                                                                                            \n');
+    console.info('Listening on 4000'.bgGreen.black);
+})
